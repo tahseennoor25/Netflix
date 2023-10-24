e85c168f8ce5a1da6be779e53a74687b8d2cb736
tahseennoor25 <121277183+tahseennoor25@users.noreply.github.com>
2023-10-24 10:14:51 +0500

    Add files via upload
    Netflix project

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+import "./index.css";
+
+
+ReactDOM.render(
+  <>
+  <h1 className="heading">List of Top 5 Netflix series in 2023</h1>
+  <Card 
+   imgsrc="https://study.com/cimages/videopreview/videopreview-full/dnou9nuyj8.jpg" 
+  category="A Netflix Orignal Series"
+title="DARK"
+link="https://www.google.com/search?sca_esv=575696865&q=fish+images&tbm=isch&source=univ&fir=X6yFXbzClInzuM%252CaDeWl3HIuOw2hM%252C_%253BfSpVZ3SPcUosiM%252CrnuyqMsGu-JtNM%252C_%253B8ID6jOGbLm3E-M%252CFsnwqZR0PdFOxM%252C_%253BhjgmXelrkOfY6M%252CJcBqV8zZsOEyrM%252C_%253BqRTf1I8PLGTUqM%252CArbNCqY3hKjsIM%252C_%253B08sJdDHvKY_D3M%252CQTvErPwG17SvJM%252C_%253Bdnf8An4LJF_RIM%252CtbPigRLAS9MPIM%252C_%253BsFQAAnXIVZqRBM%252C9oEEdvwFIa8uXM%252C_%253BMveprXRS9aQbrM%252CgYlf9ulV6wPdKM%252C_%253BopezquZrmWpvPM%252CVQSxy2mFUiHeaM%252C_%253BNEHbuOzkvnO8bM%252CZP-Cau5eWhVT3M%252C_%253BdeJgV96OWA2SNM%252CLbYWlal_oMQzmM%252C_&usg=AI4_-kT92-ZjGG372HBCYswY6DzBhhVyYg&sa=X&sqi=2&ved=2ahUKEwi-2cv8sYuCAxWZhP0HHYPEDuYQjJkEegQIFBAC&biw=1517&bih=723&dpr=0.9#imgrc=gvlJq4C2fyw1yM"
+  />
+  <Card 
+   imgsrc="https://cdn.britannica.com/13/151913-120-EA90995D/Nile-crocodile-fish.jpg" 
+   title="extra curicullar"
+  category="A Netflix Orignal Series"
+link="" 
+  />
+  <Card 
+    imgsrc="https://t3.ftcdn.net/jpg/01/56/46/50/360_F_156465042_tBW6eQdxhgAu8xWYCFX3WnSwvo3yYrYH.jpg" 
+  category="A Netflix Orignal Series"
+title="stranger things"
+link="https://www.google.com/search?q=turtle+home&tbm=isch&ved=2ahUKEwiugb73xYuCAxWWoScCHfcYAkMQ2-cCegQIABAA&oq=turtle+home&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgAEIoFEEM6CggAEIoFELEDEEM6CAgAEIAEELEDOgsIABCABBCxAxCDAVCICVimIGCNI2gAcAB4AIABwAKIAfYSkgEHMC45LjIuMZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=OxM2Ze73AZbDnsEP97GImAQ&bih=723&biw=1517#imgrc=AfkttfSEoqx7ZM"
+  />
+  <Card 
+    imgsrc="https://t4.ftcdn.net/jpg/05/57/79/91/360_F_557799107_5zzw6Q4bIuNXpChr000PKKrk8DDrZ0f6.jpg" 
+  category="A Netflix Orignal Series"
+title="stranger things"
+link="https://www.google.com/search?q=turtle+home&tbm=isch&ved=2ahUKEwiugb73xYuCAxWWoScCHfcYAkMQ2-cCegQIABAA&oq=turtle+home&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgAEIoFEEM6CggAEIoFELEDEEM6CAgAEIAEELEDOgsIABCABBCxAxCDAVCICVimIGCNI2gAcAB4AIABwAKIAfYSkgEHMC45LjIuMZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=OxM2Ze73AZbDnsEP97GImAQ&bih=723&biw=1517#imgrc=AfkttfSEoqx7ZM"
+  />
+  <Card 
+    imgsrc="https://qph.cf2.quoracdn.net/main-qimg-4df9b0d4de4ca6d3baa1ea66af8a9fc5-lq" 
+  category="A Netflix Orignal Series"
+title="stranger things"
+link="https://www.google.com/search?q=turtle+home&tbm=isch&ved=2ahUKEwiugb73xYuCAxWWoScCHfcYAkMQ2-cCegQIABAA&oq=turtle+home&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgAEIoFEEM6CggAEIoFELEDEEM6CAgAEIAEELEDOgsIABCABBCxAxCDAVCICVimIGCNI2gAcAB4AIABwAKIAfYSkgEHMC45LjIuMZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=OxM2Ze73AZbDnsEP97GImAQ&bih=723&biw=1517#imgrc=AfkttfSEoqx7ZM"
+  />
+  </>,
+  document.getElementById("root")
+);
